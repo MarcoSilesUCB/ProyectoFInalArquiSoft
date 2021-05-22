db9d0076cd469a9917be162bfe5f6d1340d1f94b
RickySandi <rickysandis@gmail.com>
2021-05-22 16:37:34 -0400

    Conexion Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase/app';
+import "firebase/firestore";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'concessionaire';
+
+  async ngOnInit() {
+    await this.initFirebase();
+  }
+
+  async initFirebase() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyC8Do90C7sTRf10tWZU_Bric4mAk5LJa-E",
+      authDomain: "concesionaria-autos.firebaseapp.com",
+      projectId: "concesionaria-autos",
+      storageBucket: "concesionaria-autos.appspot.com",
+      messagingSenderId: "1096578244105",
+      appId: "1:1096578244105:web:b8db8d95532a876db262d8",
+      measurementId: "G-6W8TYM3933"
+    };
+    // Initialize Firebase
+    await firebase.initializeApp(firebaseConfig);
+
+  }
 }
